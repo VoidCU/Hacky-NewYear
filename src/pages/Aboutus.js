678ea41d30faaafc29caf0a84420906b67e12d62
VoidCU <sarojprasadmainali@gmail.com>
2022-12-31 21:02:36 +0545

    Make use of data
    Co-authored-by: Prasaya <Prasaya@users.noreply.github.com>
    Co-authored-by: rijalsankalp <rijalsankalp@users.noreply.github.com>

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../css/aboutus.css'
 
 function Aboutus() {
   return (
-    <div>Aboutus</div>
+    <div className='main'>
+      <h1>About Us</h1>
+      <hr />
+      <div className='members'>
+      <div class="member">
+        <img src="https://avatars.githubusercontent.com/u/47562404?v=4" alt="Prasaya Acharya" class="profilepic"/>
+        <p>Prasaya Acharya</p>
+        </div>
+      <div class="member">
+        <img src="https://avatars.githubusercontent.com/u/51750221?v=4" alt="Sankalpa Rijal" class="profilepic"/>
+        <p>Sankalpa Rijal</p>
+        </div>
+      <div class="member">
+        <img src="https://avatars.githubusercontent.com/u/98652711?v=4" alt="Saroj Prasad Mainali" class="profilepic"/>
+        <p>Saroj Prasad Mainali</p>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 import '../css/Home.css'
 import arrow from '../images/arrow.png'
 
 export default function Home() {
   const [message, setMessage] = useState('');
-
   const [updated, setUpdated] = useState(message);
 
+
+
+
   const handleChange = (event) => {
-    setMessage(event.target.value);
+    if(event.target.value.length<5)
+        setMessage(event.target.value);
   };
 
   const handleClick = () => {
     setUpdated(message);
+    // var lol=fetch_data(updated);
+    // console.log(lol);
+    
   };
   const handleKeyDown=(e)=>{
     if(e.key==='Enter'){
         handleClick()
     }
   }
+  
+ 
+
+  
+
   return (
-    <div className='main'>
+    <div className='mainhome'>
         <p className='des'> Enter a year between 1900-2021:</p>
   
       <input
-        type="text"
+        type="number"
         id="message"
         name="message"
         onChange={handleChange}
         value={message}
         onKeyDown={handleKeyDown}
+        autocomplete="off" 
       />
       
-      
-      
+      <h2 id='test'>{message}</h2>
         <h2 id='test'>{updated}</h2>
     </div>
   );
