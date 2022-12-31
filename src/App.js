95df5dd6ef8f0e5c64d20b204b9b5d73d2710f55
VoidCU <sarojprasadmainali@gmail.com>
2022-12-31 08:42:40 +0545

    'preperation'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import logo from './hacky-newyear.png';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header className='App'>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Year World</h1>
+        <nav className='navBar'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Log In</li>
+        </nav>
       </header>
     </div>
   );
